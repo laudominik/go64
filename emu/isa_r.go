577dbfc89540de0f803e05d9a0628169f7893c6c
Dominik Lau <harek2388@gmail.com>
2025-01-11 14:33:32 +0100

    XOR instruction

--- emu/isa_r.go
+++ emu/isa_r.go
@@ -2,12 +2,18 @@ package emu
 
 var ISA_R_TABLE = map[uint32]InstructionCallback{
 	0b100000: r_add,
+	0b100110: r_xor,
 }
 
 var ISA_R_MNEMONIC = map[uint32]string{
 	0b100000: "ADD",
+	0b100110: "XOR",
 }
 
 func r_add(m *Machine, instr Instruction) {
 	m.cpu.r[instr.rd] = m.cpu.r[instr.rs] + m.cpu.r[instr.rt]
+}
+
+func r_xor(m *Machine, instr Instruction) {
+	m.cpu.r[instr.rd] = m.cpu.r[instr.rs] ^ m.cpu.r[instr.rt]
 }
