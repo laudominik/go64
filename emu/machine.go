4495fec0cd2d6eb9dfd6108690abbfe005f944bf
Dominik Lau <harek2388@gmail.com>
2025-01-11 12:29:30 +0100

    emulate PIF ROM effects

--- emu/machine.go
+++ emu/machine.go
@@ -9,31 +9,42 @@ import (
 type Machine struct {
 	cpu          Cpu
 	cardridgeROM []byte // Cartridge Domain 1 Address 2
+	rspData      []byte // SP DMEM
 }
 
-func (m *Machine) write(virtualAddress uint64) {
-	physicalAddress := translate(virtualAddress)
-	_ = physicalAddress
-	// TODO: call to actual hardware
+func inRange(arg, left, right uint64) bool {
+	return arg >= left && arg <= right
 }
 
-func (m *Machine) read(virtualAddress uint64) uint64 {
+func (m *Machine) write(virtualAddress uint64, value byte) {
 	physicalAddress := translate(virtualAddress)
-	_ = physicalAddress
+	if inRange(physicalAddress, 0x10000000, 0x1FBFFFFF) {
+		panic("Trying to write to cardridge ROM")
+	} else if inRange(physicalAddress, 0x04000000, 0x04000FFF) {
+		m.rspData[physicalAddress-0x04000000] = value
+	}
+}
 
-	// TODO: call to actual hardware
+func (m *Machine) read(virtualAddress uint64) byte {
+	physicalAddress := translate(virtualAddress)
+	if inRange(physicalAddress, 0x10000000, 0x1FBFFFFF) {
+		return m.cardridgeROM[physicalAddress-0x10000000]
+	} else if inRange(physicalAddress, 0x04000000, 0x04000FFF) {
+		return m.rspData[physicalAddress-0x04000000]
+	}
 	return 0
 }
 
 func (m *Machine) Reset() {
 	m.cpu.reset()
-
+	m.rspData = make([]byte, 0x1000)
 	/*
-		The first 0x1000 bytes from the cartridge are then copied to SP DMEM.
+		The first 0x1000 bytes from the cartridge are copied to SP DMEM.
 		This is implemented as a copy of 0x1000 bytes from 0xB0000000 to 0xA4000000.
 	*/
-
-	// todo: copy
+	for i := uint64(0); i < 0x1000; i++ {
+		m.write(0xA4000000+i, m.read(0xB0000000+i))
+	}
 }
 
 func (m *Machine) LoadRom(filePath string) error {
