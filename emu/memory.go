c88579b2e51b89eb33df8188eacf35a2cc25ef10
Dominik Lau <harek2388@gmail.com>
2025-01-11 12:28:38 +0100

    fixed kseg1 translation

--- emu/memory.go
+++ emu/memory.go
@@ -7,7 +7,7 @@ func translate(virtualAddress uint64) uint64 {
 	}
 	/* KSEG1 */
 	if 0xA0000000 <= virtualAddress && virtualAddress < 0xC0000000 {
-		return virtualAddress - 0x80000000
+		return virtualAddress - 0xA0000000
 	}
 	/* KUSEG, KSSEG, KSEG3 */
 	return tlbTranslate()
