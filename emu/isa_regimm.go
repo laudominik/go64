a2b3db9358bf1baed6ef31d4d1ee53f50645875f
Dominik Lau <harek2388@gmail.com>
2025-01-12 10:21:28 +0100

    added BLTZ

---
+++ emu/isa_regimm.go
@@ -0,0 +1,16 @@
+package emu
+
+var ISA_REGIMM_TABLE = map[uint32]InstructionCallback{
+	0b000000: regimm_bltz,
+}
+
+var ISA_REGIMM_MNEMONIC = map[uint32]string{
+	0b000000: "BLTZ",
+}
+
+func regimm_bltz(m *Machine, instr Instruction) {
+	if int64(m.cpu.r[instr.rs]) >= 0 {
+		return
+	}
+	m.cpu.pc += uint64(sext32(instr.imm, 16)) * 4
+}
