838eb6b8444477861336ae54fd29e697ce7595ca
Dominik Lau <harek2388@gmail.com>
2025-01-12 16:50:40 +0100

    fixed pif emulation based on disassembly

--- emu/cpu.go
+++ emu/cpu.go
@@ -22,10 +22,12 @@ func (cpu *Cpu) reset() {
 	/*
 		we don't emulate PIF ROM, just its effects
 	*/
-	cpu.r[11] = 0xFFFFFFFFA4000040
+	cpu.r[11] = 0xFFFFFFFFA4000040 // pointer to ipl3 bootcode
+	cpu.r[29] = 0xFFFFFFFFA4001FF0 // SP
+	cpu.r[31] = 0xFFFFFFFFA4001550 // Return Address
+	cpu.r[2] = 0xFFFFFFFFF58B0FBF
 	cpu.r[20] = 0x0000000000000001
 	cpu.r[22] = 0x000000000000003F
-	cpu.r[29] = 0xFFFFFFFFA4001FF0
 	cpu.cop0[1] = 0x0000001F
 	cpu.cop0[12] = 0x34000000
 	cpu.cop0[15] = 0x00000B00
