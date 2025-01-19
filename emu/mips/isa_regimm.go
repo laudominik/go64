5b312d2dd31bb852f431d4209bfa48c159615af3
Dominik Lau <harek2388@gmail.com>
2025-01-19 14:11:39 +0100

    added BGEZAL, fixed delay slot in BLTZ

--- emu/mips/isa_regimm.go
+++ emu/mips/isa_regimm.go
@@ -4,15 +4,25 @@ import "go64/emu/util"
 
 var ISA_REGIMM_TABLE = map[uint32]InstructionCallback{
 	0b000000: regimm_bltz,
+	0b010001: regimm_bgezal,
 }
 
 var ISA_REGIMM_MNEMONIC = map[uint32]string{
 	0b000000: "BLTZ",
+	0b010001: "BGEZAL",
 }
 
 func regimm_bltz(cpu *Cpu, instr Instruction) {
 	if int64(cpu.r[instr.rs]) >= 0 {
 		return
 	}
-	cpu.pc += uint64(util.Sext32(instr.imm, 16)) * 4
+	cpu.planJump(cpu.pc + uint64(util.Sext32(instr.imm, 16))*4)
+}
+
+func regimm_bgezal(cpu *Cpu, instr Instruction) {
+	if int64(cpu.r[instr.rs]) < 0 {
+		return
+	}
+	cpu.r[31] = cpu.pc + 4
+	cpu.planJump(cpu.pc + uint64(util.Sext32(instr.imm, 16))*4)
 }
