5d71c24a3faab241e488b00726e594c8bd9d1152
Dominik Lau <harek2388@gmail.com>
2025-01-19 12:52:22 +0100

    added Serial Interface stub

---
+++ emu/peripherals/serial_interface.go
@@ -0,0 +1,19 @@
+package peripherals
+
+import "fmt"
+
+type Si struct{}
+
+func (f *Si) Read(reg uint64) uint32 {
+	switch reg {
+	default:
+		panic(fmt.Sprintf("Reading from unimplemented SI register 0x%x", reg))
+	}
+}
+
+func (f *Si) Write(reg uint64, value uint32) {
+	switch reg {
+	default:
+		panic(fmt.Sprintf("Writing to unimplemented SI register 0x%x", reg))
+	}
+}
